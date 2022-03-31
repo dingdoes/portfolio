9917850451c90c6f987350ff3fa9bd8838002c9d
dingdoes <59906001+dingdoes@users.noreply.github.com>
2022-03-30 22:13:16 -0400

    Add files via upload

---
+++ darkToggle.js
@@ -0,0 +1,15 @@
+//dark mode toggle
+document.getElementById("toggle").addEventListener("click", function(){
+  document.getElementsByTagName('body')[0].classList.toggle("dark-theme");
+});
+
+//toggle headline highlight
+document.getElementById("gravity").addEventListener("click", function(){
+
+Body.setStatic(greetBox, false);
+console.log(greetBox.isStatic);
+if (greetBox.isStatic) {
+  Body.setStatic(greetBox, true);
+  console.log('ping2');
+}
+});
